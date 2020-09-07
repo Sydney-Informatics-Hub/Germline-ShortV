f34853d83abad11a2da76119f80ab0e0aa393f35
tracyc <49257820+tracychew@users.noreply.github.com>
2020-09-07 11:23:54 +1000

    Added comments

--- gatk4_hc_checklogs.sh
+++ gatk4_hc_checklogs.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: 
+# Checks gatk_hc logs for a sample
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 17/08/2020
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
+
 module load parallel/20191022
 
 echo "Checking sample: ${sample}, input: ${input}. Writing to ${logs}"
