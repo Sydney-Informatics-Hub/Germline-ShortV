9010dc8d8dcd10564770a4fcc7a7e838d6eb9460
tracyc <49257820+tracychew@users.noreply.github.com>
2020-09-07 17:32:19 +1000

    Update comments

--- gatk4_genomicsdbimport_missing_make_input.sh
+++ gatk4_genomicsdbimport_missing_make_input.sh
@@ -1,14 +1,33 @@
 #! /bin/bash
 
-# Create input file to check log files in parallel
-# For each sample, record minutes taken per interval
-# Flag any intervals with error messages
-# If there are no error messages, archive log files in a per sample tarball
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Creates input file for gatk4_genomicsdbimport_missing_run_parallel.pbs
+# Consolidates VCFs across multiple samples for missing intervals
+# If there are no missing intervals, tidys up logs by tarballing them
+# Run after gatk4_genomicsdbimport_run_parallel.pbs
+# Usage: sh gatk4_genomicsdbimport_missing_make_input.sh <cohort>
+# where <cohort> is the base name of ../<cohort>.config
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 17/08/2020
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
 
 if [ -z "$1" ]
 then
-	echo "Please run this script with the base name of your config file, e.g. sh gatk4_genomicsdbimport_missing_make_input.sh samples_batch1"
-	exit
+	echo "Please run this script with the base name of ../<cohort>.config e.g. sh gatk4_genomicsdbimport_missing_make_input.sh <cohort>"
 fi
 
 cohort=$1
