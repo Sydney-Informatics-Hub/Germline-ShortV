ca585e3840cdfc0640d60d4af3c79d836cae14fe
tracyc <49257820+tracychew@users.noreply.github.com>
2020-09-07 11:34:50 +1000

    Added comments

--- gatk4_hc_gathervcfs.sh
+++ gatk4_hc_gathervcfs.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: 
 # Merge interval level haplotypecaller VCFs per sample
 # Final output is gzipped and tabix indexed
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 17/08/2020
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
 
 module load gatk/4.1.2.0
-module load samtools/1.10
 
 set -e
 
@@ -18,10 +36,8 @@ rm -rf ${logdir}/${sample}.oe
 
 gatk GatherVcfs \
 	--arguments_file ${args} \
-	--CREATE_MD5_FILE true \
 	--MAX_RECORDS_IN_RAM 100000000 \
 	-O ${out} >> ${logdir}/${sample}.oe 2>&1
 
-wait
 
 
