752e38155abfecae471beb7bb594282f2b844f8b
tracyc <49257820+tracychew@users.noreply.github.com>
2020-09-07 10:50:14 +1000

    Added comments

--- gatk4_hc.sh
+++ gatk4_hc.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Runs gatk4 HaplotypeCaller with
+# gatk4_hc_run_parallel.pbs
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 17/08/2020
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
+
 # Run GATK HC using scatter-gather method
 
 module load gatk/4.1.2.0
