ee18988d2ebc092af2cdaaa61dcadbd60c7bdb86
tracyc <49257820+tracychew@users.noreply.github.com>
2020-09-08 11:30:13 +1000

    Added comments

--- gatk4_genotypegvcfs.sh
+++ gatk4_genotypegvcfs.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Jointly genotype samples with GenotypeGVCFs
+# Used by gatk4_genotypegvcfs_run_parallel.pbs script and 
+# operates on one genomic interval per task
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 17/08/2020
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
+
+
 # -all-sites option does not work and give any output 
 # GATK4 uses gnomAD variants, not dbSNP variants
 # No threading option
 
 module load gatk/4.1.2.0
-module load samtools/1.10
 
 ref=`echo $1 | cut -d ',' -f 1`
 cohort=`echo $1 | cut -d ',' -f 2`
@@ -39,6 +61,5 @@ echo "$(date) : Finished GATK 4 joing genotyping with GenotypeGVCFs for: ${out}"
 
 ## Options to explore
 # --sample-ploidy,-ploidy:Integer : for different sex chromosomes, Mt
-# --interval-padding - but genomicsdbimport needs to include padded sites
 # --include-non-variant-sites : up to the researcher
 # --dbsnp
