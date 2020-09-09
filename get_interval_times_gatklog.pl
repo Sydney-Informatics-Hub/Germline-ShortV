5230e728e998ee9437d16edd5fe7f490ec96f0ce
tracyc <49257820+tracychew@users.noreply.github.com>
2020-09-09 19:29:21 +1000

    Added comments

--- get_interval_times_gatklog.pl
+++ get_interval_times_gatklog.pl
@@ -3,13 +3,27 @@
 use strict;
 use warnings;
 
-# We want to see how long each interval in HC took to compute
-# to identify regions that are difficult to call and take longer walltime
-# if these are "predicatable" regions, e.g. unplaced/unlocalized contigs
-# this script gets duration per task using GATK logs
-# will tell us time for all intervals in 1 sample, but not task + duration 
-
-#my $logdir="/scratch/gq19/Batch1/Germline-ShortV-batch1-1/Logs/gatk4_hc/10651-B";
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: 
+# Supplementary script for gatk4_hc_checklogs_run_parallel.sh
+# Checks duration for each gatk4_hc.sh task (1 sample, 1 interval)
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 17/08/2020
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
 
 my $logdir=$ARGV[0];
 
@@ -24,6 +38,4 @@ foreach my $file (@files){
 	$timelog=~ m/([0-9]+\.[0-9]+) minutes\.\n$/;
 	my $duration=$1;
 	print "$interval\t$duration\n";
-	#print "$interval\t$duration\n";
-	
 }
