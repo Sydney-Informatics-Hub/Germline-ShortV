4ca2e1b12bb791bb448018d96551418f1eb5be58
tracyc <49257820+tracychew@users.noreply.github.com>
2021-07-22 14:15:24 +1000

    Update get_interval_times_gatklog.pl

--- get_interval_times_gatklog.pl
+++ get_interval_times_gatklog.pl
@@ -4,25 +4,29 @@ use strict;
 use warnings;
 
 #########################################################
-#
-# Platform: NCI Gadi HPC
-# Description: 
-# Supplementary script for gatk4_hc_checklogs_run_parallel.sh
-# Checks duration for each gatk4_hc.sh task (1 sample, 1 interval)
-# Author: Tracy Chew
-# tracy.chew@sydney.edu.au
-# Date last modified: 17/08/2020
-#
-# If you use this script towards a publication, please acknowledge the
-# Sydney Informatics Hub (or co-authorship, where appropriate).
-#
-# Suggested acknowledgement:
-# The authors acknowledge the scientific and technical assistance
-# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
-# Hub and resources and services from the National Computational
-# Infrastructure (NCI), which is supported by the Australian Government
-# with access facilitated by the University of Sydney.
-#
+# 
+# Platform: NCI Gadi HPC 
+# Usage: 
+# Version: 2.0 
+# 
+# For more details see: https://github.com/Sydney-Informatics-Hub/Germline-ShortV 
+# 
+# If you use this script towards a publication, support us by citing: 
+# 
+# Suggest citation: 
+# Sydney Informatics Hub, Core Research Facilities, University of Sydney, 
+# 2021, The Sydney Informatics Hub Bioinformatics Repository, <date accessed>, 
+# https://github.com/Sydney-Informatics-Hub/Germline-ShortV 
+# 
+# Please acknowledge the Sydney Informatics Hub and the facilities: 
+# 
+# Suggested acknowledgement: 
+# The authors acknowledge the technical assistance provided by the Sydney 
+# Informatics Hub, a Core Research Facility of the University of Sydney 
+# and the Australian BioCommons which is enabled by NCRIS via Bioplatforms 
+# Australia. The authors acknowledge the use of the National Computational 
+# Infrastructure (NCI) supported by the Australian Government. 
+# 
 #########################################################
 
 my $logdir=$ARGV[0];
